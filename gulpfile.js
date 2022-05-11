2f7c63a3a6aae088f8256e4fc93b4ccf8915c486
dlrpfl <70948673+dlrpfl@users.noreply.github.com>
2022-05-11 16:14:33 +0900

    작업중

--- gulpfile.js
+++ gulpfile.js
@@ -68,60 +68,54 @@ function server(done) {
 }
 
 // html
-function copyHtml() {
-	return new Promise(resolve => {
-		src(paths.html)
-			.pipe(plumber({ errorHandler: onError }))
-			.pipe(fileinclude({
-				prefix: '@@',
-				basepath: '@file',
-				context: {
-					
-				}
-			}))
-			.pipe(cached('html'))
-			.pipe(dest(dist.html))
-			.on('end', () => {
-				browsersync.reload();
-			});
-		resolve();
-	});
+function copyHtml(done) {
+	return src(paths.html)
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file',
+			context: {
+				
+			}
+		}))
+		.pipe(cached('html'))
+		.pipe(dest(dist.html))
+		.on('end', () => {
+			browsersync.reload();
+		}),
+	done();
 }
 exports.copyHtml = copyHtml;
 
 // css
-function compileScss() {
-	return new Promise(resolve => {
-		const options = {
-			outputStyle: "compressed",  // 컴파일 스타일: expanded, compressed
-			indentType: "tab",      // 들여쓰기 스타일: space(default), tab
-			indentWidth: 1,           // 들여쓰기 칸 수 (Default : 2)
-			precision: 6,             // 컴파일 된 CSS 의 소수점 자리수 (Type : Integer , Default : 5)
-			sourceComments: false     // 코멘트 제거 여부 (Default : false)
-		};
-		src(paths.css, { since: lastRun(compileScss) })
-			.pipe(plumber({ errorHandler: onError }))
-			.pipe(dependents())
-			.pipe(sourcemaps ? sourcemaps.init() : noop())
-			.pipe(sass(options).on('error', sass.logError))
-			.pipe(autoprefixer())
-			.pipe(sourcemaps ? sourcemaps.write() : noop())
-			.pipe(dest(dist.css))
-			.pipe(browsersync.reload({ stream: true }));
-		resolve();
-	});
+function compileScss(done) {
+	const options = {
+		outputStyle: "compressed",  // 컴파일 스타일: expanded, compressed
+		indentType: "tab",      // 들여쓰기 스타일: space(default), tab
+		indentWidth: 1,           // 들여쓰기 칸 수 (Default : 2)
+		precision: 6,             // 컴파일 된 CSS 의 소수점 자리수 (Type : Integer , Default : 5)
+		sourceComments: false     // 코멘트 제거 여부 (Default : false)
+	};
+	return src(paths.css, { since: lastRun(compileScss) })
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(dependents())
+		.pipe(sourcemaps ? sourcemaps.init() : noop())
+		.pipe(sass(options).on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(sourcemaps ? sourcemaps.write() : noop())
+		.pipe(dest(dist.css))
+		.pipe(browsersync.reload({ stream: true })),
+	done();
 }
 exports.compileScss = compileScss;
 
 // js
-function copyJs() {
-	return new Promise(resolve => {
-		src(paths.js, { since: lastRun(copyJs) })
-			.pipe(plumber({ errorHandler: onError }))
-			.pipe(dest(dist.js))
-			.pipe(browsersync.reload({ stream: true }));
-		resolve();
-	});
+function copyJs(done) {
+	return src(paths.js, { since: lastRun(copyJs) })
+		.pipe(plumber({ errorHandler: onError }))
+		.pipe(dest(dist.js))
+		.pipe(browsersync.reload({ stream: true })),
+	done();
 }
 exports.copyJs = copyJs;
 
@@ -145,6 +139,7 @@ function watchs(done) {
 	watch([paths.html, paths.inc], copyHtml);
 	watch(paths.css, compileScss);
 	watch(paths.js, copyJs);
+	watch(paths.img, image);
 	// watch(['src/assets/images/ico/*.png'], series(cleanSprite, sprite)); // 스프라이트 이미지 감시용
 	done();
 }
@@ -200,4 +195,4 @@ function cleanSprite(done) {
 }
 exports.cleanSprite = cleanSprite;
 
-exports.default = series(copyHtml, compileScss, copyJs, server, watchs);
+exports.default = series(parallel(copyHtml, compileScss, copyJs, image), server, watchs);
